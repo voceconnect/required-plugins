0371fe32a2c2b921ce2c17378a34ad0fdab0e4bd
= <mpretty@voceconnect.com>
2013-11-05 13:44:46 -0500

    renaming plugin and fixing function declaration

--- loader.php
+++ loader.php
@@ -1,12 +1,12 @@
 <?php
 
-if(!defined('WP_DEBUG') || !WP_DEBUG) {
+if(!defined('WP_DEBUG') || !WP_DEBUG || defined('SKIP_REQUIRED_PLUGINS')) {
 	return;
 }
 
-add_action('muplugins_loaded', 'voce_mu_plugins_load_plugins');
+add_action('muplugins_loaded', 'voce_required_plugins_load_plugins');
 
-voce_mu_plugins_load_plugins() {
+function voce_required_plugins_load_plugins() {
 	do_action('wp_load_dependency', 'limit-login-attempts');
 	do_action('wp_load_dependency', 'force-strong-passwords', 'slt-force-strong-passwords');
 }
