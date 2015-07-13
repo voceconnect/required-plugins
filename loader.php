432143e22dc43f4fbd9b533ddbcb2650d36e57af
= <mpretty@voceconnect.com>
2015-07-13 09:01:25 -0400

    adding separate bootstrap to check that wp is loaded, adding disable-xml-rpc-pingback

--- loader.php
+++ loader.php
@@ -1,11 +1,8 @@
 <?php
 use Voce\PluginLoader\PluginLoader;
 
-if( ! defined('ABSPATH') || ( defined('WP_DEBUG') && WP_DEBUG ) || ( defined('SKIP_REQUIRED_PLUGINS') && SKIP_REQUIRED_PLUGINS ) ) {
-	return;
-}
-
 $pluginLoader = new PluginLoader();
 $pluginLoader->registerHooks();
 do_action( Voce\PluginLoader\PluginLoader::LOAD_ACTION, 'wp-fail2ban' );
 do_action( Voce\PluginLoader\PluginLoader::LOAD_ACTION, 'force-strong-passwords', 'slt-force-strong-passwords' );
+do_action( Voce\PluginLoader\PluginLoader::LOAD_ACTION, 'disable-xml-rpc-pingback' );
