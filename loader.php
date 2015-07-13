18137dc60c9983179d25ea92945ec2564d3f08df
= <mpretty@voceconnect.com>
2015-07-13 12:26:07 -0400

    waiting load plugins on muplugins_loaded action

--- loader.php
+++ loader.php
@@ -1,4 +1,9 @@
 <?php
-do_action( 'wp_load_dependency', 'wp-fail2ban' );
-do_action( 'wp_load_dependency', 'force-strong-passwords', 'slt-force-strong-passwords' );
-do_action( 'wp_load_dependency', 'disable-xml-rpc-pingback' );
+
+add_action('muplugins_loaded', 'voce_required_plugins_load_plugins');
+
+function voce_required_plugins_load_plugins() {
+	do_action( 'wp_load_dependency', 'wp-fail2ban' );
+	do_action( 'wp_load_dependency', 'force-strong-passwords', 'slt-force-strong-passwords' );
+	do_action( 'wp_load_dependency', 'disable-xml-rpc-pingback' );
+}
